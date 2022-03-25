3b5d75e64236217a40b575218e0feb14e8f405c1
강원종 <mikeyid77@gmail.com>
2022-03-25 12:00:07 +0900

    Commit - 220325
    Toast Test

--- Assets/Project/Scripts/Test.cs
+++ Assets/Project/Scripts/Test.cs
@@ -1,31 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
-using System;
 using UnityEngine;
-using Microsoft.Toolkit.Uwp.Notifications;
-using Microsoft.AppCenter;
-using Microsoft.AppCenter.Unity;
-using Microsoft.AppCenter.Unity.Analytics;
-using Microsoft.AppCenter.Unity.Crashes;
 
 
 
 public class Test : MonoBehaviour
 {
-    void Start()
-    {
-        //Type[] serviceArray = { typeof(Analytics), typeof(Crashes) };
-        //AppCenter.StartFromLibrary(serviceArray);
-    }
-
     public void OnClick()
     {
-        Debug.Log("Alert");
-
-        ToastContentBuilder content = new ToastContentBuilder();
-        content.AddArgument("action", "viewConversation")
-               .AddArgument("conversationId", 9813)
-               .AddText("Andrew sent you a picture")
-               .AddText("Check this out, The Enchantments in Washington!");
+#if ENABLE_WINMD_SUPPORT
+        DLL.ToastLibrary.ToastGenerator generator = new DLL.ToastLibrary.ToastGenerator();
+        generator.ShowToast();
+#endif
     }
 }

3ea45005e7db956b843be77a041ee5b18d446da2
강원종 <mikeyid77@gmail.com>
2022-03-28 16:26:19 +0900

    Commit - 220328
    add ToastLibrary

--- Assets/Project/Scripts/Test.cs
+++ Assets/Project/Scripts/Test.cs
@@ -10,6 +10,18 @@ public class Test : MonoBehaviour
 {
     public void OnClick()
     {
-        ToastGenerator.ShowToast();
+        List<ToastText> binding = new List<ToastText>();
+        binding.Add(new ToastText("TITLE", ToastTextType.TITLE));
+        binding.Add(new ToastText("text1", ToastTextType.NORMAL));
+        binding.Add(new ToastText("text2", ToastTextType.NORMAL));
+        binding.Add(new ToastText("attribution", ToastTextType.ATTRIBUTION));
+
+        List<ToastImage> image = new List<ToastImage>();
+        image.Add(new ToastImage("https://picsum.photos/364/180?image=200", ToastImageType.HERO));
+        image.Add(new ToastImage("https://picsum.photos/48?image=400", ToastImageType.SQUARE_LOGO));
+        //image.Add(new ToastImage("https://picsum.photos/48?image=400", ToastImageType.CIRCLE_LOGO));
+        image.Add(new ToastImage("https://picsum.photos/360/202?image=600", ToastImageType.INLINE));
+
+        ToastGenerator.GenerateToast(binding, image, null);
     }
 }

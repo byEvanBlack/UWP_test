32088a4b9f62bad6b7e002e61d8ff03ed1f2ac92
강원종 <mikeyid77@gmail.com>
2022-03-30 17:02:54 +0900

    Commit - 220330
    Tile, Badge 추가

--- Assets/Project/Scripts/Test.cs
+++ Assets/Project/Scripts/Test.cs
@@ -2,28 +2,57 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Metaverse.Plugins;
 
 
 
 public class Test : MonoBehaviour
 {
-    public void OnClick()
+    public Button _toastButton;
+    public Button _tileButton;
+    public Button _badgeButton;
+    public Button _clearButton;
+
+    private void Start()
+    {
+        _toastButton.onClick.AddListener(OnToast);
+        _tileButton.onClick.AddListener(OnTile);
+        _badgeButton.onClick.AddListener(OnBadge);
+        _clearButton.onClick.AddListener(OnClear);
+    }
+
+    public void OnToast()
     {
         string path = "file:///C:/Working/RnD/UWP_test/Assets/Project/Resources/";
+        new ToastGenerator()
+            .SetImage(path + "img_hero.png", eTOAST_IMAGE.HERO)
+            .SetImage(path + "img_logo.png", eTOAST_IMAGE.CIRCLE_LOGO)
+            .SetText("TITLE", eTOAST_TEXT.TITLE)
+            .SetText("text", eTOAST_TEXT.NORMAL)
+            .SetImage(path + "img_inline.png", eTOAST_IMAGE.INLINE)
+            .Generate();
+    }
 
-        List<ToastText> binding = new List<ToastText>();
-        binding.Add(new ToastText("TITLE", ToastTextType.TITLE));
-        binding.Add(new ToastText("text1", ToastTextType.NORMAL));
-        binding.Add(new ToastText("text2", ToastTextType.NORMAL));
-        binding.Add(new ToastText("attribution", ToastTextType.ATTRIBUTION));
+    public void OnTile()
+    {
+        string path = "file:///C:/Working/RnD/UWP_test/Assets/Project/Resources/";
+        new TileGenerator()
+            .SetSize(eTILE_SIZE.WIDE)
+            .SetText("BASE", eTILE_TEXT.BASE)
+            .SetText("caption", eTILE_TEXT.CAPTION)
+            .SetImage(path + "img_inline.png", eTILE_IMAGE.CIRCLE)
+            .SetImage(path + "img_hero.png", eTILE_IMAGE.BACKGROUND)
+            .Generate();
+    }
 
-        List<ToastImage> image = new List<ToastImage>();
-        image.Add(new ToastImage(path + "img_hero.png", ToastImageType.HERO));
-        //image.Add(new ToastImage(path + "img_logo.png", ToastImageType.SQUARE_LOGO));
-        image.Add(new ToastImage(path + "img_logo.png", ToastImageType.CIRCLE_LOGO));
-        image.Add(new ToastImage(path + "img_inline.png", ToastImageType.INLINE));
+    public void OnBadge()
+    {
+        new BadgeGenerator().SetBadge(27).Generate();
+    }
 
-        ToastGenerator.GenerateToast(binding, image, null);
+    public void OnClear()
+    {
+        NotificationClear.ClearAll();
     }
 }
